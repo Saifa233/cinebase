8773fb8b0c141372cc39f80cafebd769d04156f0
Saifa233 <saifazeem99@gmail.com>
2025-04-24 13:01:38 +0500

    Db url added

--- db/index.ts
+++ db/index.ts
@@ -2,8 +2,9 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-if (!process.env.NEXT_PUBLIC_DATABASE_URL) {
+const NEXT_PUBLIC_DATABASE_URL = 'postgresql://neondb_owner:npg_AShKvqjo17dG@ep-noisy-sunset-a8runxwh-pooler.eastus2.azure.neon.tech/neondb?sslmode=require'
+if (NEXT_PUBLIC_DATABASE_URL) {
     throw Error('NEXT_PUBLIC_DATABASE_URL is not defined');
 }
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+const sql = neon(NEXT_PUBLIC_DATABASE_URL);
 export const db = drizzle(sql);
